883cfe6cb195f5c05d15c2dcc2c2cc9969c486c5
Meryl Rastall <merylrastall@gmail.com>
2020-11-12 15:05:07 +0000

    debugged

--- src/note-controller.js
+++ src/note-controller.js
@@ -2,24 +2,24 @@
 
 class NoteController {
 
-  constructor(noteList = new NoteList()) {
+    constructor(noteList = new NoteList()) {
 
-    this.noteList = noteList
-    this.noteList.add('Favourite drink: seltzer');
-    this.view = new NoteListView(this.noteList)
+        this.noteList = noteList
+        this.noteList.add('Favourite drink: seltzer');
+        this.view = new NoteListView(this.noteList)
 
-  }
+    }
 
-  insertHTMLString() {
+    insertHTMLString() {
 
-    document.querySelector('#app').innerHTML = this.view.returnHTMLString()
-    // document.querySelector('#app').textContent = this.view.returnHTMLString()
-    // document.getElementById('app').innerHTML = this.view.returnHTMLString()
-    // document.getElementById('app').textContent = this.view.returnHTMLString()
+        //  document.querySelector('#app').innerHTML = this.view.returnHTMLString()
+        // document.querySelector('#app').textContent = this.view.returnHTMLString()
+        document.getElementById('app').innerHTML = this.view.returnHTMLString()
+            // document.getElementById('app').textContent = this.view.returnHTMLString()
 
-  }
+    }
 
 }
 
-noteController = new NoteController()
+var noteController = new NoteController;
 noteController.insertHTMLString();
